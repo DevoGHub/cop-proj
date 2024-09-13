081920275ea09dd5393dfc310295e613514a8756
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2024-09-13 21:50:52 +0530

    Final commit

--- src/components/upload/index.js
+++ src/components/upload/index.js
@@ -1,21 +1,27 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
-import { FilePreview } from "../filePreview";
+import { Link, useParams } from "react-router-dom";
 import { FileUpload } from "../fileUpload";
 import { ProfileView } from "../profileView";
 import { SearchComp } from "../searchComp";
+import { TextField } from "../textField";
 
 export const Upload = () => {
     const [patient, setPatient] = useState();
     let { patientId } = useParams();
     const [file, setFile] = useState();
+    const [value, setValue] = useState();
 
     return (
         <div>
             <SearchComp setPatient={setPatient} patientId={patientId} />
             <ProfileView patient={patient} />
-            <FileUpload fieldId="something" label="File" value={file} setValue={setFile} />
-            <FilePreview alt="lorem ipsum" url={'https://media-cdn.tripadvisor.com/media/photo-s/0c/bb/a3/97/predator-ride-in-the.jpg'} label={"something"} />
-        </div>
+            {
+                patient && <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+                    <FileUpload fieldId="something" label="File" value={file} setValue={setFile} />
+                    <TextField fieldId={`id-${() => Math.random() * 120000}`} label="Field Value" placeholder="Enter Field Value" value={value} setValue={setValue} />
+                    <Link to={`/view/${patient.id}`} >View Records</Link>
+                </div>
+            }
+        </div >
     );
 }
